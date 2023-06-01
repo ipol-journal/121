44110fc083e7067a4bb24d101b094352d0e84342
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2023-06-01 18:31:13 +0530

    write to algo_info.txt

--- run.py
+++ run.py
@@ -22,6 +22,9 @@ p = {}
 logfile = open('log.txt', 'w')
 
 if args.action == "Interpolate":
+    #write Interpolate=True in algo_info.txt
+    with open('algo_info.txt', 'w') as file:
+        file.write("Interpolate=1")
     # In this run mode, the interpolation is performed directly on the
     # selected image, and the estimated contours are also shown.
     
